fdd613ec760c0bf9b7f353308a85b93c746d1ea4
angelomarcelino <amc.marcelino.cordeiro@gmail.com>
2018-08-08 23:06:31 -0300

    Etapas 4 e 5

--- Pratica 01/Qt_Proj/ListaCandidatos.h
+++ Pratica 01/Qt_Proj/ListaCandidatos.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <cstring>
 #include <sstream>
+#include <fstream>
 
 using namespace std;
 
@@ -14,10 +15,24 @@ class ListaCandidatos {
 public:
     NoCandidato *head;
 
+    //Construtores
     inline ListaCandidatos(): head(NULL) {}
+    ListaCandidatos(string arq): head(NULL) {
+        ifstream fcin(arq);
+
+        string dados;
+        getline(fcin,dados);
+        cout << "criacao de lista de candidatos de: " << dados << endl;
+
+        while(getline(fcin,dados))
+        {
+            adicioneComoHead(new Candidato(dados));
+        }
+    }
+
+    //Métodos
     inline void adicioneComoHead(Candidato *c) {head = new NoCandidato(c,head);}
     inline bool estaVazia() {return head==NULL;}
-
     int tamanho() const {
         NoCandidato *aux = head;
         int cont(0);
@@ -28,12 +43,42 @@ public:
         }
         return cont;
     }
-
     string toString() const {
         stringstream stream;
         stream << head->toString();
         return stream.str();
     }
+    bool remove(string nome, string sobrenome){
+        NoCandidato* it = head;
+        bool achou = false;
+        int cont = 0;
+        while(!achou && it!=NULL){
+            if(it->conteudo->igual(nome,sobrenome))
+            {
+                achou = true;
+                if(cont==0)
+                {
+                    head = it->next;
+                    delete it;
+                }
+                else
+                {
+                    NoCandidato* aux = head;
+                    for(int i=0; i<cont-1; i++)
+                    {
+                        aux = aux->next;
+                    }
+                    aux->next = it->next;
+                    delete it;
+                }
+
+            }
+            else it = it->next;
+            cont ++;
+        }
+        return achou;
+    }
+
 };
 
 #endif // LISTACANDIDATOS_H
--- Pratica 01/Qt_Proj/Qt_Proj.pro
+++ Pratica 01/Qt_Proj/Qt_Proj.pro
@@ -15,7 +15,7 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    TesteEx3b.cpp
+    TesteEx5.cpp
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
